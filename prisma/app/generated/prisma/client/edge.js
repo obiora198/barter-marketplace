4462302c3363c07c11e5f0586c6749701ac132df
Emmanuel Obiora <emmanuelobiora11@gmail.com>
2025-04-26 13:54:32 +0100

    remove native query binary

--- prisma/app/generated/prisma/client/edge.js
+++ prisma/app/generated/prisma/client/edge.js
@@ -190,7 +190,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../../../.env",
     "schemaEnvPath": "../../../../../.env"
   },
   "relativePath": "../../../..",
--- prisma/app/generated/prisma/client/index.js
+++ prisma/app/generated/prisma/client/index.js
@@ -191,7 +191,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../../../.env",
     "schemaEnvPath": "../../../../../.env"
   },
   "relativePath": "../../../..",
